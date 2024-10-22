dafc112cfc11590459c424c1d2c0761e2d02fea9
BethEvansNHSE <bethan.evans15@nhs.net>
2024-10-22 14:16:20 +0100

    edits to text

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("home_page.py", title="Welcome!"),
-    #st.Page("des_page.py", title="Run Simulation"),
-    #st.Page("lsoa_map.py", title = "LSOA")
+    st.Page("df.py", title="Summary table of incidents"),
+    st.Page("eda.py", title = "Graphs...")
 ])
 
 #st.logo("hsma_logo.png")
