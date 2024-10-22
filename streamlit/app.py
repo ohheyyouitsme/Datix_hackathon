22888401424f2e4dca3bf7d931f946481d8951c7
Atremis <flamingphoneix@googlemail.com>
2024-10-22 15:35:03 +0100

    Added: Additonal changes

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("home_page.py", title="Welcome!"),
+    st.Page("Visuals.py", title="Visualisation"),
+    st.Page("Common_Words.py", title="Common Words")
     #st.Page("des_page.py", title="Run Simulation"),
     #st.Page("lsoa_map.py", title = "LSOA")
 ])
