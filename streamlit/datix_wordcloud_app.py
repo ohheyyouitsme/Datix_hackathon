e35af6e113e92a699cf72c11e2bf32e62b165761
Igris <92097024+FushigiOoka@users.noreply.github.com>
2024-10-22 14:57:00 +0100

    UPDATE: multiple worldclouds and downloads

--- streamlit/datix_wordcloud_app.py
+++ streamlit/datix_wordcloud_app.py
@@ -24,13 +24,16 @@ user_text_input = st.text_input("Insert text for wordcloud.")
 #"Action Taken (at the time of the incident)",
 #"Summary of Actions Taken (Investigation)",
 #"Lessons Learned"
+uploaded_file = st.file_uploader("Choose a file", type=['csv','xlsx'])
+if uploaded_file is not None:
+    #read csv
+    df=pd.read_csv(uploaded_file)
+   #read xls or xlsx
+   #df1=pd.read_excel(uploaded_file)
+else:
+    st.warning("you need to upload a csv or excel file.")
 
-uploaded_file = st.file_uploader(
-    "Please upload a text file for wordcloud",
-    type=['txt'], key = "1" # <1>
-    )
 st.text(uploaded_file.name)
-
 
 
 uploaded_image_mask = st.file_uploader(
@@ -41,21 +44,35 @@ if uploaded_image_mask is not None:
     bytes_data = uploaded_image_mask.getvalue()
     # Show the image filename and image.
     st.image(bytes_data)
-st.text(f'filename: {uploaded_image_mask.name}')
+    st.text(f'filename: {uploaded_image_mask.name}')
 
-uploaded_image_mask = np.array(Image.open(uploaded_image_mask.name))
+    uploaded_image_mask = np.array(Image.open(uploaded_image_mask.name))
 #uploaded_image_mask = Image.open(uploaded_image_mask)
 #img_array = np.array(uploaded_image_mask)
-choose_colour = st.selectbox(
 
+
+chosen_specialty = st.selectbox(
+    "Choose a Speciality",
+    options=df['Speciality'].drop_duplicates().tolist())
+
+chosen_category = st.selectbox(
+    "Choose a Category",
+    options=df['Category'].drop_duplicates().tolist())
+
+chosen_divsion = st.selectbox(
+    "Choose a Division",
+    options=df['Division'].drop_duplicates().tolist())
+
+
+choose_colour = st.selectbox(
    "Choose a colour scheme",
     ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r']
 #    ["Black", "Reds", "Wisteria","BuPu_r"],
 
 )
 
-if uploaded_file is not None or user_text_input is not None:
-    full_text = uploaded_file.read()
+def create_word_cloud(data_column):
+    full_text = ' '.join(data_column)
     full_text = str(full_text)
     #stopwords
     stopwords = set(STOPWORDS)
@@ -72,8 +89,7 @@ if uploaded_file is not None or user_text_input is not None:
     #image generation
     fig, ax = plt.subplots()
     if uploaded_image_mask is not None:
-        
-
+    
         wordcloud = WordCloud(#font_path = 'font\\GothamMedium.ttf',
                             background_color = 'white',
                             min_word_length =3, 
@@ -106,14 +122,64 @@ if uploaded_file is not None or user_text_input is not None:
     plt.show()
     st.pyplot(fig)
 
-filename = 'my_word_cloud.png'
-plt.savefig(filename)
+tab1, tab2, tab3, tab4 = st.tabs([
+    "What Happened?",
+    "Lessons Learned",
+    "Actions Taken (during incident)",
+    "Summary of Actions Taken (Investigation)",
+])
+if uploaded_file is not None or user_text_input is not None:
+    with tab1:
+        create_word_cloud(df['What Happened?'])
 
-with open(filename, "rb") as img:
-    btn = st.download_button(
-        label="Download image",
-        data=img,
-        file_name=filename,
-        mime="image/png"
-    )
+        filename1 = 'output/my_word_cloud1.png'
+        plt.savefig(filename1)
+
+        with open(filename1, "rb") as img:
+            btn = st.download_button(
+                label="Download image",
+                data=img,
+                file_name=filename1,
+                mime="image/png"
+            )
+    with tab2:
+        create_word_cloud(df['Lessons Learned'])
+        
+        filename2 = 'output/my_word_cloud2.png'
+        plt.savefig(filename2)
+
+        with open(filename2, "rb") as img:
+            btn = st.download_button(
+                label="Download image",
+                data=img,
+                file_name=filename2,
+                mime="image/png"
+            )
+    with tab3:
+        create_word_cloud(df['Action Taken (at the time of the incident)'])
+
+        filename3 = 'output/my_word_cloud3.png'
+        plt.savefig(filename3)
+
+        with open(filename3, "rb") as img:
+            btn = st.download_button(
+                label="Download image",
+                data=img,
+                file_name=filename3,
+                mime="image/png"
+            )
+    with tab4:
+        create_word_cloud(df['Summary of Actions Taken (Investigation)'])
+
+        filename4 = 'output/my_word_cloud4.png'
+        plt.savefig(filename4)
+
+        with open(filename4, "rb") as img:
+            btn = st.download_button(
+                label="Download image",
+                data=img,
+                file_name=filename4,
+                mime="image/png"
+            )
+
 
