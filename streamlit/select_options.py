2663a5596ba35b0d9562d6e421d7eafeb9dc072e
zosiaboj-nhs <zosia.bojanowski@uhbw.nhs.uk>
2024-10-22 14:29:39 +0100

    UPD: change data source

--- streamlit/select_options.py
+++ streamlit/select_options.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # load main df
-datix_df = pd.read_csv("../data/Test_data.csv")
+datix_df = pd.read_csv("../data/1924_Total.csv")
 
 # select division
 selected_divisions = st.multiselect(
@@ -21,7 +21,3 @@ selected_specialties = st.multiselect(
 
 # filter on specialties
 edited_df = datix_df[datix_df["Speciality"].isin(selected_specialties)]
-
-
-# datix_df["Category"].columns
-# datix_df["Sub Category"].columns
