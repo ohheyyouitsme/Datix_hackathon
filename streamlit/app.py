74b2f601e2b9feb82bd3eb1834b317de0aa64800
BethEvansNHSE <bethan.evans15@nhs.net>
2024-10-22 12:02:44 +0100

    updates to the files in the Streamlit folder

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("home_page.py", title="Welcome!"),
-    st.Page("des_page.py", title="Run Simulation"),
-    st.Page("lsoa_map.py", title = "LSOA")
+    #st.Page("des_page.py", title="Run Simulation"),
+    #st.Page("lsoa_map.py", title = "LSOA")
 ])
 
-st.logo("hsma_logo.png")
+#st.logo("hsma_logo.png")
 
 pg.run()
--- streamlit/home_page.py
+++ streamlit/home_page.py
@@ -1,18 +1,9 @@
 import streamlit as st
 
-st.write("Here's the simulation map and calculation")
+st.write("Analysis of the Trust's Datix data")
 
-if 'iat_calls' not in st.session_state:
-    st.session_state.iat_calls = None 
-if 'iat_walkins' not in st.session_state:
-    st.session_state.iat_walkins = None
-
-if st.session_state.iat_calls is None:
-    st.write("There is no average time because the model has not yet run")
-else:
-    st.write(f"The IAT calls number is {st.session_state.iat_calls:.1f}")
-
-if st.session_state.iat_walkins is None:
-    st.write("There is no walkins average because the model has not yet run")
-else:
-    st.write(f"The IAT walkins number is {st.session_state.iat_walkins:.1f}")
+# example code for setting session states, in case it is needed
+# if 'iat_calls' not in st.session_state:
+#     st.session_state.iat_calls = None 
+# if 'iat_walkins' not in st.session_state:
+#     st.session_state.iat_walkins = None
