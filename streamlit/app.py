7adfbb89e9ccf4649fc71252e95ee2b61fcd74cf
BethEvansNHSE <bethan.evans15@nhs.net>
2024-10-22 14:22:30 +0100

    updated app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,7 +3,8 @@ import streamlit as st
 pg = st.navigation([
     st.Page("home_page.py", title="Welcome!"),
     st.Page("df.py", title="Summary table of incidents"),
-    st.Page("eda.py", title = "Graphs...")
+    st.Page("eda.py", title = "Graphs..."),
+    st.Page("datix_wordcloud_app.py", title = "Wordclouds...")
 ])
 
 #st.logo("hsma_logo.png")
