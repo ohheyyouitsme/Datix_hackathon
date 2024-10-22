7a49c703019766154fdc3de4f6014fe9f3792d21
BethEvansNHSE <bethan.evans15@nhs.net>
2024-10-22 14:51:44 +0100

    updated: table filters and 'in progress' wheel added

--- streamlit/df.py
+++ streamlit/df.py
@@ -1,12 +1,49 @@
 import pandas as pd
 import streamlit as st
+import time
 
-datix_df = pd.read_csv(r"..\data\1924_Total.csv")
+
+# initiate session states
+if 'selected_divisions' not in st.session_state:
+    st.session_state.selected_divisions = None
+
+if 'selected_specialties' not in st.session_state:
+    st.session_state.selected_specialties = None
+
+# load main df
+datix_df = pd.read_csv("../data/1924_Total.csv")
+
+# select division
+selected_divisions = st.multiselect(
+    "Select division(s)",
+    datix_df["Division"].drop_duplicates().sort_values().tolist(),
+    default=st.session_state.selected_divisions
+)
+
+# filter on division
+edited_df = datix_df[datix_df["Division"].isin(selected_divisions)]
+
+# select specialty
+selected_specialties = st.multiselect(
+    "Select specialty(s)",
+    edited_df["Speciality"].drop_duplicates().sort_values().tolist(),
+    default=st.session_state.selected_specialties
+)
+
+# filter on specialties
+edited_df = datix_df[datix_df["Speciality"].isin(selected_specialties)]
+
+# set session states
+st.session_state.selected_divisions = selected_divisions
+st.session_state.selected_specialties = selected_specialties
+
+with st.spinner(text="In progress"):
+    time.sleep(2)
 
 ## dataframe
 st.dataframe(
-    # We pass in the dataframe but limit it to these 5 columns
-    datix_df[[ 'Category',
+    # Pass edited df
+    edited_df[[ 'Category',
     'Sub Category',
     'Result',
     'Actual Harm',
